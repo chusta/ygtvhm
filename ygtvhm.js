e8aaf67830e6da9fc9a0fce4add68710ecff28a1
chusta <chusta@users.noreply.github.com>
2020-12-26 00:41:52 -0500

    add data/1606107600-1606626000s.json

--- ygtvhm.js
+++ ygtvhm.js
@@ -117,6 +117,7 @@ d3.json("data/1603684800-1604203200s.json"),
 d3.json("data/1604293200-1604811600s.json"),
 d3.json("data/1604898000-1605416400s.json"),
 d3.json("data/1605502800-1606021200s.json"),
+d3.json("data/1606107600-1606626000s.json"),
 /* WEEKEND */
 ];
 draw(dataWeekday, "_weekday", yWeekday);
