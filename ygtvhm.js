466b8502793c94c79954648cb80ab063c465d9bd
chusta <chusta@users.noreply.github.com>
2021-07-18 20:00:29 -0400

    add data/1624852800-1625371200s.json

--- ygtvhm.js
+++ ygtvhm.js
@@ -83,6 +83,7 @@ d3.json("data/1622433600-1622952000s.json"),
 d3.json("data/1623038400-1623556800s.json"),
 d3.json("data/1623643200-1624161600s.json"),
 d3.json("data/1624248000-1624766400s.json"),
+d3.json("data/1624852800-1625371200s.json"),
 /* WEEKEND */
 ];
 
